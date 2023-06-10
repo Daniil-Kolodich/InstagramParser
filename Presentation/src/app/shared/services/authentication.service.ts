09290cd5025e972c6ae6e5bfc32dbe505ead4eba
Daniil-Kolodich <danik.kolodich@mail.ru>
2023-06-10 23:22:12 +0300

    shared module and auth service

--- Presentation/src/app/shared/services/authentication.service.ts
+++ Presentation/src/app/shared/services/authentication.service.ts
@@ -1,20 +1,35 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class AuthenticationService {
-	public ymp: string | undefined = undefined;
+	private readonly httpClient = inject(HttpClient);
+	private readonly _authorized$ = new BehaviorSubject<boolean>(false);
+
+	// TODO: should this be handled with destroy?
+	public authorized$(): Observable<boolean> {
+		return this._authorized$.asObservable();
+	}
+
+	public login(request: LoginUserRequest): void {
+		this.httpClient.get('').subscribe(
+			(result) => this._authorized$.next(true),
+			(error) => this._authorized$.next(false)
+		);
+	}
 }
 
 // LoginUserRequest(string Email, string Password);
-type LoginUserRequest = {
+export type LoginUserRequest = {
 	Email: string;
 	Password: string;
 };
 
 // RegisterUserRequest(string UserName, string Email, string Password)
-type RegisterUserRequest = {
+export type RegisterUserRequest = {
 	UserName: string;
 	Email: string;
 	Password: string;
--- Presentation/src/app/shared/shared.module.ts
+++ Presentation/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FormControl, FormGroup } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+	declarations: [],
+	imports: [CommonModule],
 })
-export class SharedModule { }
+export class SharedModule {}
+
+export type ControlsOf<T extends Record<string, unknown>> = {
+	[K in keyof T]: T[K] extends Record<any, unknown> ? FormGroup<ControlsOf<T[K]>> : FormControl<T[K]>;
+};
