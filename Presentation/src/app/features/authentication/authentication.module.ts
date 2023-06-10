564005a439f04dd4768ae899d31f16c1b8a05e38
Daniil-Kolodich <danik.kolodich@mail.ru>
2023-06-10 23:22:49 +0300

    basic login page

--- Presentation/src/app/features/authentication/authentication.module.ts
+++ Presentation/src/app/features/authentication/authentication.module.ts
@@ -3,6 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LogInComponent } from './components/log-in/log-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDividerModule } from '@angular/material/divider';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const moduleName = 'authentication/';
 
@@ -10,10 +19,19 @@ const moduleName = 'authentication/';
 	declarations: [LogInComponent, SignUpComponent],
 	imports: [
 		CommonModule,
+		MatButtonModule,
+		MatIconModule,
+		MatTooltipModule,
 		RouterModule.forChild([
 			{ path: moduleName + 'login', component: LogInComponent },
 			{ path: moduleName + 'signup', component: SignUpComponent },
 		]),
+		MatCardModule,
+		MatProgressBarModule,
+		MatDividerModule,
+		ReactiveFormsModule,
+		MatFormFieldModule,
+		MatInputModule,
 	],
 })
 export class AuthenticationModule {}
--- Presentation/src/app/features/authentication/components/log-in/log-in.component.ts
+++ Presentation/src/app/features/authentication/components/log-in/log-in.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthenticationService, LoginUserRequest } from '../../../../shared/services/authentication.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ControlsOf } from '../../../../shared/shared.module';
+import { filter, takeUntil } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { DestroyableComponent } from '../../../../shared/components/destroyable.component';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-log-in',
-  templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.sass']
+	selector: 'app-log-in',
+	templateUrl: './log-in.component.html',
+	styleUrls: ['./log-in.component.sass'],
 })
-export class LogInComponent {
+export class LogInComponent extends DestroyableComponent implements OnInit {
+	private readonly authenticationService = inject(AuthenticationService);
+	private readonly formBuilder: FormBuilder = inject(FormBuilder);
+	private readonly router: Router = inject(Router);
 
+	public form: FormGroup<ControlsOf<LoginUserRequest>> | undefined;
+	public loading = false;
+
+	public ngOnInit(): void {
+		this.authenticationService
+			.authorized$()
+			.pipe(
+				tap(() => (this.loading = false)),
+				filter((value) => value),
+				takeUntil(this.destroy$)
+			)
+			.subscribe(() => this.router.navigate(['']));
+
+		this.form = this.formBuilder.nonNullable.group({
+			Email: '',
+			Password: '',
+		});
+	}
+
+	public onSubmit(): void {
+		if (!this.form || this.form.invalid) {
+			return;
+		}
+
+		this.loading = true;
+		this.authenticationService.login(this.form.getRawValue());
+	}
 }
