9e61926c9334efc8907c472fa8948d84bab8bce9
Daniil-Kolodich <danik.kolodich@mail.ru>
2023-06-17 22:15:37 +0300

    added basic dashboard template

---
+++ Presentation/src/app/features/dashboard/components/request-creation/request-creation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-creation',
+  templateUrl: './request-creation.component.html',
+  styleUrls: ['./request-creation.component.sass']
+})
+export class RequestCreationComponent {
+
+}
--- Presentation/src/app/features/dashboard/dashboard.module.ts
+++ Presentation/src/app/features/dashboard/dashboard.module.ts
@@ -4,6 +4,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { Route } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { authenticatedGuard } from '../../shared/guards/authenticated.guard';
+import { RequestCreationComponent } from './components/request-creation/request-creation.component';
+import { RequestHistoryComponent } from './components/request-history/request-history.component';
+import { RequestStatisticsComponent } from './components/request-statistics/request-statistics.component';
 
 export const dashboardRoute: Route = {
 	path: 'dashboard',
@@ -12,7 +15,7 @@ export const dashboardRoute: Route = {
 };
 
 @NgModule({
-	declarations: [DashboardComponent],
+	declarations: [DashboardComponent, RequestCreationComponent, RequestHistoryComponent, RequestStatisticsComponent],
 	imports: [CommonModule, SharedModule],
 })
 export class DashboardModule {}
