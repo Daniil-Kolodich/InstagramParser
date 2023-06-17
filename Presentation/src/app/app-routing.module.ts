533f1389489eb85f37b94e3a3ed7a368ad70c8ae
Daniil-Kolodich <danik.kolodich@mail.ru>
2023-06-17 22:50:45 +0300

    basic routing to request creation page

--- Presentation/src/app/app-routing.module.ts
+++ Presentation/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authenticationRoute } from './features/authentication/authentication.module';
-import { dashboardRoute } from './features/dashboard/dashboard.module';
+import { dashboardRoutes } from './features/dashboard/dashboard.module';
 
-const routes: Routes = [authenticationRoute, dashboardRoute];
+const routes: Routes = [authenticationRoute].concat(dashboardRoutes);
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
--- Presentation/src/app/features/dashboard/dashboard.module.ts
+++ Presentation/src/app/features/dashboard/dashboard.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { Route } from '@angular/router';
+import { RouterLink, Routes } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { authenticatedGuard } from '../../shared/guards/authenticated.guard';
 import { RequestCreationComponent } from './components/request-creation/request-creation.component';
 import { RequestHistoryComponent } from './components/request-history/request-history.component';
 import { RequestStatisticsComponent } from './components/request-statistics/request-statistics.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
-export const dashboardRoute: Route = {
-	path: 'dashboard',
-	component: DashboardComponent,
-	canActivate: [authenticatedGuard],
-};
+export const dashboardRoutes: Routes = [
+	{
+		path: 'dashboard',
+		component: DashboardComponent,
+		canActivate: [authenticatedGuard],
+	},
+	{
+		path: 'dashboard/create',
+		component: RequestCreationComponent,
+		canActivate: [authenticatedGuard],
+	},
+];
 
 @NgModule({
 	declarations: [DashboardComponent, RequestCreationComponent, RequestHistoryComponent, RequestStatisticsComponent],
-	imports: [CommonModule, SharedModule],
+	imports: [CommonModule, SharedModule, MatCardModule, MatButtonModule, RouterLink],
 })
 export class DashboardModule {}
