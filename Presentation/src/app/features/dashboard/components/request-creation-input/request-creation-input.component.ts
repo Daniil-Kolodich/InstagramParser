fc1ed1107e374221988f4e438f01ca58bb0feab4
Daniil-Kolodich <danik.kolodich@mail.ru>
2023-06-20 01:48:44 +0300

    fixes & improvements

--- Presentation/src/app/features/dashboard/components/request-creation-input/request-creation-input.component.ts
+++ Presentation/src/app/features/dashboard/components/request-creation-input/request-creation-input.component.ts
@@ -1,13 +1,20 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
 import { SubscriptionSource } from '../../../../shared/services/dashboard.service';
-import { FormArray, FormControl } from '@angular/forms';
-import { catchError, debounceTime, filter, Observable, of, switchMap, takeUntil } from 'rxjs';
+import {
+	AbstractControl,
+	FormArray,
+	FormControl,
+	NonNullableFormBuilder,
+	ValidationErrors,
+	ValidatorFn,
+} from '@angular/forms';
+import { catchError, debounceTime, filter, finalize, of, startWith, Subject, switchMap, takeUntil } from 'rxjs';
 import { DestroyableComponent } from '../../../../shared/components/destroyable.component';
-import { HttpClient } from '@angular/common/http';
 import {
 	GetInstagramAccountResponse,
 	InstagramAccountService,
 } from '../../../../shared/services/instagram-account.service';
+import { ObservableResults } from '../../../../shared/types';
 
 @Component({
 	selector: 'app-request-creation-input[form][type]',
@@ -15,30 +22,55 @@ import {
 	styleUrls: ['./request-creation-input.component.sass'],
 })
 export class RequestCreationInputComponent extends DestroyableComponent implements OnInit {
-	private readonly httpClient = inject(HttpClient);
+	private readonly formBuilder = inject(NonNullableFormBuilder);
 	private readonly instagramAccountService = inject(InstagramAccountService);
 
 	@Input() public type!: string;
 	@Input() public form!: [Source: FormControl<SubscriptionSource>, Accounts: FormArray<FormControl<string>>];
 
-	public readonly searchInput = new FormControl<GetInstagramAccountResponse | string>('');
+	public readonly searchInput = new FormControl<GetInstagramAccountResponse | string>('', [correctInstagramAccount]);
 	public readonly SubscriptionSource = SubscriptionSource;
-	public account$?: Observable<GetInstagramAccountResponse | undefined>;
-
+	public account$?: ObservableResults<GetInstagramAccountResponse>;
+	private notFound$ = new Subject<unknown | null>();
+	private loading$ = new Subject<boolean>();
 	public ngOnInit(): void {
-		this.account$ = this.searchInput.valueChanges.pipe(
-			takeUntil(this.destroy$),
-			debounceTime(300),
-			filter(isString),
-			switchMap((value) =>
-				this.instagramAccountService.getByUserName(value).pipe(catchError(() => of(undefined)))
-			)
-		);
+		this.account$ = {
+			Error: this.notFound$,
+			Loading: this.loading$,
+			Value: this.searchInput.valueChanges.pipe(
+				takeUntil(this.destroy$),
+				debounceTime(300),
+				filter(isString),
+				switchMap((value) => {
+					this.notFound$.next(null);
+					this.loading$.next(true);
+
+					return this.instagramAccountService.getByUserName(value).pipe(
+						startWith({ userName: 'danon', id: '111' }),
+						finalize(() => this.loading$.next(false)),
+						catchError(() => {
+							this.notFound$.next({});
+							return of(null);
+						})
+					);
+				})
+			),
+		};
+	}
+
+	public addAccount() {
+		console.log('form value', this.searchInput.value, this.searchInput.value);
 	}
 
 	public displayWith(account: GetInstagramAccountResponse): string {
 		return account.userName;
 	}
+}
+
+function correctInstagramAccount(
+	control: AbstractControl<GetInstagramAccountResponse | string>
+): ValidationErrors | null {
+	return isString(control.value) ? { invalidAccount: { value: control.value } } : null;
 }
 
 function isString<T>(value: T | string | null): value is string {
--- Presentation/src/app/shared/services/instagram-account.service.ts
+++ Presentation/src/app/shared/services/instagram-account.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
@@ -9,9 +9,10 @@ export class InstagramAccountService {
 	private readonly httpClient = inject(HttpClient);
 	private readonly url = 'instagramAccount/';
 
-	public getByUserName(userName: string): Observable<GetInstagramAccountResponse | undefined> {
+	public getByUserName(userName: string): Observable<GetInstagramAccountResponse> {
 		const query = new HttpParams({ fromObject: { userName: userName } });
-		return this.httpClient.get<GetInstagramAccountResponse>(this.url + 'GetByName', { params: query });
+		return of();
+		// return this.httpClient.get<GetInstagramAccountResponse>(this.url + 'GetByName', { params: query });
 	}
 }
 
