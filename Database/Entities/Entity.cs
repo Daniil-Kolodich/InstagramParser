3a8d984afedddb5b1c508f0673261dec5f798bc6
Daniil-Kolodich <danik.kolodich@mail.ru>
2023-06-25 16:41:40 +0300

    more cleanup

--- Database/Entities/Entity.cs
+++ Database/Entities/Entity.cs
@@ -2,7 +2,6 @@ namespace Database.Entities;
 
 public abstract class Entity
 {
-    // TODO: check may be init is working too
     public int Id { get; private set; }
     public DateTime CreatedWhen { get; internal set; }
     public DateTime? UpdatedWhen { get;  internal set; } = null;
